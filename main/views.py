c1d0966e1ae2d53b7c24957eead983a49f64bd32
vissutagunawan <vglim3653@gmail.com>
2024-10-04 12:35:12 +0700

    Tutorial 5 done

--- main/views.py
+++ main/views.py
@@ -10,16 +10,17 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
     context = {
         'npm': '2306214656',
         'name': request.user.username,
         'class': 'PBP D',
-        'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login']
     }
 
@@ -38,11 +39,11 @@ def create_mood_entry(request):
     return render(request, 'create_mood_entry.html', context)
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -76,7 +77,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse('main:show_main'))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-        
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")        
     else:
         form = AuthenticationForm(request)
     context = {'form': form}
@@ -107,3 +109,15 @@ def delete_mood(request, id):
     mood = MoodEntry.objects.get(pk=id)
     mood.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood"))
+    feelings = strip_tags(request.POST.get("feelings"))
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(mood=mood, feelings=feelings, mood_intensity=mood_intensity, user=user)
+    new_mood.save()
+    return HttpResponse(b"CREATED", status=201)
